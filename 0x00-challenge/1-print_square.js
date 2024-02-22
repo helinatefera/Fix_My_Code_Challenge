e7623063f746645f110ed6c9cf8a88ebf4c45603
helinatefera <helinatefera1212@gmail.com>
2024-02-22 12:02:36 -0800

    readme

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-/*
-    Print a square with the character #
-    
-    The size of the square must be the first argument 
-    of the program.
 
-    change in size = parseInt(process.argv[2], 16)
-*/
 
 
 if (process.argv.length <= 2) {
